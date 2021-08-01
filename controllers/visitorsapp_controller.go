769603d5ccc3b9ed8576cc8a89af51a3f695a69d
段如笑 <duanruxiao@duanruxiaodeMacBook-Pro.local>
2021-08-01 19:54:55 +0800

    0.0.9: rbac

--- controllers/visitorsapp_controller.go
+++ controllers/visitorsapp_controller.go
@@ -43,6 +43,10 @@ type VisitorsAppReconciler struct {
 //+kubebuilder:rbac:groups=example.com.my.domain,resources=visitorsapps,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=example.com.my.domain,resources=visitorsapps/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=example.com.my.domain,resources=visitorsapps/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
